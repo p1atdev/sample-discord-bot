b900e8ac84979d7c9ad0f8f6ee923427359e68b6
unknown <git@p1at.dev>
2022-11-09 16:43:48 +0900

    fix bot

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 # This example requires the 'message_content' intent.
+from dotenv import load_dotenv
+
+load_dotenv()
 
 import discord
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
 
-client.run('your token here')
+
+client.run(os.environ["DISCORD_TOKEN"])
